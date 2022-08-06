15bac79101f6dabc2c5bc8937d6178e54b80d0e2
zjs6631 <zacharyjonessmith@gmail.com>
2022-08-06 14:51:34 -0400

    commiting so I can work on home PC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import {Todo} from './Todo.js'
+import {Todo} from './Todo.js';
+import {mainPage} from './generateMainPage';
 
 console.log("js connected");
-
+const mainContainer = document.getElementById('content');
 let obj = new Todo("title", "lame", "now", "high", "notes")
 
 console.log(obj)
+console.log("updating")
+
+mainContainer[0].appendChild(mainPage());
